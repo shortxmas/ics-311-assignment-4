42bef03e62506ad41d729c9a27357dd752ddd632
Leighton Miguel <leim56@hawaii.rr.com>
2024-07-20 21:20:34 -1000

    Code comments

--- classes/Tourism.py
+++ classes/Tourism.py
@@ -2,17 +2,18 @@
 
 class Tourism:
     def __init__(self, graph, tourist, start_island):
-        self.graph = graph
-        self.tourist = tourist
-        self.start_island = start_island
-        self.visited_islands = set()
-        self.total_time = 0
+        self.graph = graph  # The graph representing the islands and connections
+        self.tourist = tourist  # The tourist object
+        self.start_island = start_island  # The starting island for the tour
+        self.visited_islands = set()  # Set to keep track of visited islands
+        self.total_time = 0  # Total travel time
 
+    # Greedy algorithm to find next Island
     def find_next_island(self, current_island):
-        # Find the next island with the most experiences within the shortest travel time
-        next_island = None
-        min_travel_time = float('inf')
+        next_island = None  # The next island to visit
+        min_travel_time = float('inf')  # Initialize to infinity
 
+        # Find the closest unvisited island
         for island_name, travel_time in current_island.connections.items():
             if island_name not in self.visited_islands and travel_time < min_travel_time:
                 next_island = self.graph.islands[island_name]
@@ -21,16 +22,16 @@ class Tourism:
         return next_island, min_travel_time
 
     def tour(self):
-        current_island = self.graph.islands[self.start_island]
-        self.visited_islands.add(current_island.name)
+        current_island = self.graph.islands[self.start_island]  # Start at the initial island
+        self.visited_islands.add(current_island.name)  # Mark the starting island as visited
 
         while True:
             next_island, travel_time = self.find_next_island(current_island)
             if not next_island:
-                break 
+                break  # Exit if no unvisited islands are left
 
-            self.visited_islands.add(next_island.name)
-            self.total_time += travel_time
-            current_island = next_island
+            self.visited_islands.add(next_island.name)  # Mark the island as visited
+            self.total_time += travel_time  # Add travel time
+            current_island = next_island  # Move to the next island
 
-        return self.visited_islands, self.total_time
+        return self.visited_islands, self.total_time  # Return the visited islands and total travel time
